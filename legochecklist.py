093621fa1de21c3485828fb153b9b630bb179496
John Goewert <john@goewert.org>
2023-02-07 21:00:02 -0500

    Move components out to different files for easier editing

--- legochecklist.py
+++ legochecklist.py
@@ -1,102 +1,18 @@
 import sys
-from PySide6.QtWidgets import QApplication, QLabel, QWidget, QGridLayout, QPushButton, QSizePolicy, QScrollArea, QVBoxLayout, QSpinBox, QLayout, QLineEdit, QMainWindow
-from PySide6.QtGui import QPixmap, QIcon
-from PySide6.QtCore import Qt, QSize, QRect, QPoint, QMargins, QTimer, QRunnable, Slot, Signal, QObject, QThreadPool
-from PySide6 import QtCore, QtGui, QtUiTools
+from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtCore import QRunnable, Slot, Signal, QObject, QThreadPool
 import requests
 import json
 import os, os.path
 from ui_mainwindow import Ui_MainWindow
 import traceback
 
-#import urllib.request
+from piece import Piece
+from flowlayout import FlowLayout
 
-part_width = 200
+
 REBRICKABLE_API_KEY = ""
 sort_algorithm="name"
-
-class Piece:
-    stylesheet = """QSpinBox {
-  border: 1px solid #ABABAB;
-  border-radius: 3px;
-}
-
-QSpinBox::down-button  {
-  subcontrol-origin: margin;
-  subcontrol-position: center left;
-  image: url(:/icons/leftArrow.png);
-  background-color: #ABABAB;
-  left: 1px;
-  height: 24px;
-  width: 24px;
-}
-
-QSpinBox::up-button  {
-  subcontrol-origin: margin;
-  subcontrol-position: center right;
-  image: url(:/icons/rightArrow.png);
-  background-color: #ABABAB;
-  right: 1px;
-  height: 24px;
-  width: 24px;
-}"""
-
-    widget = None
-    def __init__(self, num, color, img, qty, name):
-        self.num = num
-        self.name = name
-        self.color = color
-        self.img = img
-        self.qty = qty
-        block = QGridLayout()
-        block.addWidget(QLabel(self.num + " - " + self.name),0,0,1,2)
-        partImage = self.getPixmap()
-        partImage.setAlignment(Qt.AlignmentFlag.AlignHCenter)
-        block.addWidget(partImage,1,0,3,1)
-        quantityLabel = QLabel("Want\n" + str(self.qty))
-        quantityLabel.setAlignment(Qt.AlignmentFlag.AlignHCenter)
-        block.addWidget(quantityLabel,1,1)
-        haveLabel = QLabel("Have")
-        haveLabel.setAlignment(Qt.AlignmentFlag.AlignHCenter)
-        block.addWidget(haveLabel,2,1)
-        spinbox = QSpinBox()
-        spinbox.setMaximum(self.qty)
-        spinbox.valueChanged.connect(self.spinboxchanged)
-        spinbox.setStyleSheet(self.stylesheet)
-        spinbox.setMaximumWidth(100)
-        spinbox.setAlignment(Qt.AlignmentFlag.AlignHCenter)
-        block.addWidget(spinbox,3,1)
-        
-        self.widget = QWidget()
-        self.widget.setStyleSheet("background-color: white;")
-        self.widget.setBaseSize(80,part_width)
-        self.widget.setMaximumWidth(part_width)
-        self.widget.setMinimumWidth(part_width)
-        self.widget.setLayout(block)
-
-    def getPixmap(self):
-        imgicon = QLabel()
-        if self.img is not None:
-            filepath = os.path.join("images/", str(os.path.basename(self.img)))
-            if os.path.exists(filepath):
-                image = QPixmap(filepath)
-            else:
-                image = QPixmap()
-        else:
-            image = QPixmap()
-        imgicon.setPixmap(image)
-        imgicon.setMaximumSize(64,64)
-        imgicon.setScaledContents(True)
-        return imgicon
-
-    def spinboxchanged(self, value_as_int):
-        if value_as_int == self.qty:
-            self.widget.setStyleSheet("background-color: green;")
-        else:
-            self.widget.setStyleSheet("background-color: white;")
-
-    def getWidget(self):
-        return self.widget
 
 def sort_by_name(list):
     return list["part"]["name"]
@@ -143,96 +59,6 @@ class Worker(QRunnable):
             self.signals.result.emit(result)  # Return the result of the processing
         finally:
             self.signals.finished.emit()  # Done
-
-
-class FlowLayout(QLayout):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-
-        if parent is not None:
-            self.setContentsMargins(QMargins(0, 0, 0, 0))
-
-        self._item_list = []
-
-    def __del__(self):
-        item = self.takeAt(0)
-        while item:
-            item = self.takeAt(0)
-
-    def addItem(self, item):
-        self._item_list.append(item)
-
-    def count(self):
-        return len(self._item_list)
-
-    def itemAt(self, index):
-        if 0 <= index < len(self._item_list):
-            return self._item_list[index]
-
-        return None
-
-    def takeAt(self, index):
-        if 0 <= index < len(self._item_list):
-            return self._item_list.pop(index)
-
-        return None
-
-    def expandingDirections(self):
-        return Qt.Orientation(0)
-
-    def hasHeightForWidth(self):
-        return True
-
-    def heightForWidth(self, width):
-        height = self._do_layout(QRect(0, 0, width, 0), True)
-        return height
-
-    def setGeometry(self, rect):
-        super(FlowLayout, self).setGeometry(rect)
-        self._do_layout(rect, False)
-
-    def sizeHint(self):
-        return self.minimumSize()
-
-    def minimumSize(self):
-        size = QSize()
-
-        for item in self._item_list:
-            size = size.expandedTo(item.minimumSize())
-
-        size += QSize(2 * self.contentsMargins().top(), 2 * self.contentsMargins().top())
-        return size
-
-    def _do_layout(self, rect, test_only):
-        x = rect.x()
-        y = rect.y()
-        line_height = 0
-        spacing = self.spacing()
-
-        for item in self._item_list:
-            style = item.widget().style()
-            layout_spacing_x = style.layoutSpacing(
-                QSizePolicy.PushButton, QSizePolicy.PushButton, Qt.Horizontal
-            )
-            layout_spacing_y = style.layoutSpacing(
-                QSizePolicy.PushButton, QSizePolicy.PushButton, Qt.Vertical
-            )
-            space_x = spacing + layout_spacing_x
-            space_y = spacing + layout_spacing_y
-            next_x = x + item.sizeHint().width() + space_x
-            if next_x - space_x > rect.right() and line_height > 0:
-                x = rect.x()
-                y = y + line_height + space_y
-                next_x = x + item.sizeHint().width() + space_x
-                line_height = 0
-
-            if not test_only:
-                item.setGeometry(QRect(QPoint(x, y), item.sizeHint()))
-
-            x = next_x
-            line_height = max(line_height, item.sizeHint().height())
-
-        return y + line_height - rect.y()
 
 class MainWindow(QMainWindow):
     def __init__(self):
---
+++ piece.py
@@ -0,0 +1,89 @@
+from PySide6.QtWidgets import QLabel, QWidget, QGridLayout, QSpinBox
+from PySide6.QtCore import Qt
+from PySide6.QtGui import QPixmap
+import os, os.path
+
+class Piece:
+    stylesheet = """QSpinBox {
+  border: 1px solid #ABABAB;
+  border-radius: 3px;
+}
+
+QSpinBox::down-button  {
+  subcontrol-origin: margin;
+  subcontrol-position: center left;
+  image: url(:/icons/leftArrow.png);
+  background-color: #ABABAB;
+  left: 1px;
+  height: 24px;
+  width: 24px;
+}
+
+QSpinBox::up-button  {
+  subcontrol-origin: margin;
+  subcontrol-position: center right;
+  image: url(:/icons/rightArrow.png);
+  background-color: #ABABAB;
+  right: 1px;
+  height: 24px;
+  width: 24px;
+}"""
+
+    widget = None
+    part_width = 200
+
+    def __init__(self, num, color, img, qty, name):
+        self.num = num
+        self.name = name
+        self.color = color
+        self.img = img
+        self.qty = qty
+        block = QGridLayout()
+        block.addWidget(QLabel(self.num + " - " + self.name),0,0,1,2)
+        partImage = self.getPixmap()
+        partImage.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+        block.addWidget(partImage,1,0,3,1)
+        quantityLabel = QLabel("Want\n" + str(self.qty))
+        quantityLabel.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+        block.addWidget(quantityLabel,1,1)
+        haveLabel = QLabel("Have")
+        haveLabel.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+        block.addWidget(haveLabel,2,1)
+        spinbox = QSpinBox()
+        spinbox.setMaximum(self.qty)
+        spinbox.valueChanged.connect(self.spinboxchanged)
+        spinbox.setStyleSheet(self.stylesheet)
+        spinbox.setMaximumWidth(100)
+        spinbox.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+        block.addWidget(spinbox,3,1)
+        
+        self.widget = QWidget()
+        self.widget.setStyleSheet("background-color: white;")
+        self.widget.setBaseSize(80, self.part_width)
+        self.widget.setMaximumWidth(self.part_width)
+        self.widget.setMinimumWidth(self.part_width)
+        self.widget.setLayout(block)
+
+    def getPixmap(self):
+        imgicon = QLabel()
+        if self.img is not None:
+            filepath = os.path.join("images/", str(os.path.basename(self.img)))
+            if os.path.exists(filepath):
+                image = QPixmap(filepath)
+            else:
+                image = QPixmap()
+        else:
+            image = QPixmap()
+        imgicon.setPixmap(image)
+        imgicon.setMaximumSize(64,64)
+        imgicon.setScaledContents(True)
+        return imgicon
+
+    def spinboxchanged(self, value_as_int):
+        if value_as_int == self.qty:
+            self.widget.setStyleSheet("background-color: green;")
+        else:
+            self.widget.setStyleSheet("background-color: white;")
+
+    def getWidget(self):
+        return self.widget
